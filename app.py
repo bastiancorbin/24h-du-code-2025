bfa5afc7f47c0cb9266f243968982cd3a5403607
Clement-Chevreuil <clement.chevreuil@outlook.com>
2025-03-22 22:59:47 +0100

    Remove deprecated files and shaders from the three.js library

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, render_template, send_from_directory
 from bot import send_request
 
 app = Flask(__name__)
@@ -10,6 +10,15 @@ def index():
 @app.route('/receptionist', methods=['GET'])
 def chat_with_receptionist():
     return send_request(request.args.get('message'))
+
+# New route to serve the Three.js animation
+@app.route('/animation')
+def animation():
+    return render_template('animation.html')
+
+@app.route('/assets/<path:filename>')
+def serve_assets(filename):
+    return send_from_directory('assets', filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
